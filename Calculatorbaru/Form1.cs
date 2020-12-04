e47273449a5becff9b05b9e19320ebbf1c6e9427
fillajunior <pantailovina@gmail.com>
2020-12-04 09:40:39 +0800

    Add project files.

---
+++ Calculatorbaru/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatorbaru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.OnCreate += new Form2.CreateUpdateEventHandler(this.FrmEntry_OnCreate);
+            f2.ShowDialog();
+        }
+
+        private void lstboxHasil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void FrmEntry_OnCreate(string nama, string operasi, double a, double b, double hasils)
+        {
+            lstboxHasil.Items.Add("Hasil " + nama + " " + a + " " + operasi + b + "=" + hasils);
+        }
+    }
+}
---
+++ Calculatorbaru/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatorbaru
+{
+    public partial class Form2 : Form
+    {
+        public delegate void CreateUpdateEventHandler(string nama, string operasi, double a, double b, double hasils);
+        public event Form2.CreateUpdateEventHandler OnCreate;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Double hasils, NilaiA, NilaiB;
+            string nama = this.cmbOperasi.Text.ToLower();
+            string operasi = string.Empty;
+            hasils = 0;
+            NilaiA = Double.Parse(this.txtNilaiA.Text);
+            NilaiB = Double.Parse(this.txtNilaiB.Text);
+
+
+            if (this.cmbOperasi.SelectedIndex == -1)
+            {
+                MessageBox.Show("Silahkan Pilih Dulu Operasinya!!!");
+            }
+            else if (this.cmbOperasi.SelectedIndex == 0)
+            {
+                hasils = NilaiA + NilaiB;
+
+            }
+            else if (this.cmbOperasi.SelectedIndex == 1)
+            {
+                hasils = NilaiA - NilaiB;
+            }
+            else if (this.cmbOperasi.SelectedIndex == 2)
+            {
+                hasils = NilaiA * NilaiB;
+            }
+            else if (this.cmbOperasi.SelectedIndex == 3)
+            {
+                hasils = NilaiA / NilaiB;
+            }
+
+            this.OnCreate(nama, operasi, NilaiA, NilaiB, hasils);
+        }
+    }
+}
